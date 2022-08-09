a877030bab5750f3ce2b0e5121b1ffb862840fe4
mao <1296193245@qq.com>
2022-08-09 21:24:17 +0800

    java_design_patterns_liskov_substitution_principle

--- src/main/java/mao/after/RectangleDemo.java
+++ src/main/java/mao/after/RectangleDemo.java
@@ -1,5 +1,6 @@
 package mao.after;
 
+
 /**
  * Project name(项目名称)：java设计模式_里氏代换原则
  * Package(包名): mao.after
@@ -15,5 +16,44 @@ package mao.after;
 
 public class RectangleDemo
 {
+    /**
+     * 拓宽，直到宽度比长度还大，正方形会死循环
+     *
+     * @param rectangle Rectangle对象
+     */
+    public static void resize(Rectangle rectangle)
+    {
+        while (rectangle.getWidth() <= rectangle.getLength())
+        {
+            rectangle.setWidth(rectangle.getWidth() + 1);
+        }
+    }
 
+    /**
+     * 打印长和宽
+     *
+     * @param quadrilateral Quadrilateral实现类对象
+     */
+    public static void printf(Quadrilateral quadrilateral)
+    {
+        System.out.println("长度：" + quadrilateral.getLength());
+        System.out.println("宽度：" + quadrilateral.getWidth());
+    }
+
+
+    public static void main(String[] args)
+    {
+        Rectangle rectangle = new Rectangle(9, 3);
+        printf(rectangle);
+        resize(rectangle);
+        printf(rectangle);
+
+        //----
+
+        Square square = new Square();
+        square.setSide(13);
+        printf(square);
+        //因为无法使用resize(square)，解决了此问题
+        //resize(square);
+    }
 }
